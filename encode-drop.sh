ca45a84e096e784316c909cc278001d23f78271a
Dave Burke <dburke84@gmail.com>
2020-09-01 22:04:28 -0500

    Allow passing extra args to encode

--- encode-drop.sh
+++ encode-drop.sh
@@ -8,6 +8,10 @@ function debug() {
 	fi
 }
 
+function usage() {
+	echo "$(basename ${0})" [input dir] [output dir] [flags]
+}
+
 TMP_DIR="/var/tmp/encode.$$.$RANDOM"
 debug "Using '${TMP_DIR}' for temporary output"
 mkdir -p "${TMP_DIR}"
@@ -22,17 +26,23 @@ if [[ -z "${DEBUG}" ]]; then
 	trap cleanup ERR
 fi
 
-DROP="./test"
 if [[ -d "${1}" ]]; then
 	DROP="${1}"
+	shift
+else
+	usage && exit 1
 fi
 debug "Looking for files in '${DROP}'"
 
-DEST="./dest"
-if [[ -d "${2}" ]]; then
-	DEST="${2}"
+if [[ -d "${1}" ]]; then
+	DEST="${1}"
+	shift
+else
+	usage && exit 1
 fi
 debug "Files will be moved to '${DEST}'"
+
+ARGS=$@
 
 function init() {
 	local dir="$(dirname "${1#${DROP}/}")"
@@ -56,7 +66,7 @@ if type doencode > /dev/null; then
 	debug "Using hooked doencode function"
 else
 	function doencode() {
-		cp -v "${INPUT_FILE}" "${TEMP_FILE}"
+		transcode --output "${TEMP_FILE}" --add-subtitle=all $ARGS "${INPUT_FILE}"
 	}
 fi
 
